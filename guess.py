4b7be69f07cd0cb7bf833a2e3db3d05bec0d004b
Xuequn Le <LEXUEQUN@gmail.com>
2021-01-16 15:44:34 -0700

    2nd Edition

--- guess.py
+++ guess.py
@@ -1,6 +1,10 @@
 import random
 r = random.randint(1,100)
+
+count = 0
+
 while True:
+	count = count + 1 #count += 1
 	num = input("guess the number: ")
 	num = int(num)
 	if num == r:
@@ -10,3 +14,4 @@ while True:
 		print('Your number is larger than the correct answer')
 	elif num < r:
 		print ('Your number is smaller than the correct answer')
+	print('You have tried ', count, 'time(s)')
